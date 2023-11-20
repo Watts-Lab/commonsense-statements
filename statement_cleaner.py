caa701ec92a8e831b8afe57cf2dcf6740522468b
Amir Nakhaei <6696894+amirrr@users.noreply.github.com>
2023-11-20 17:34:02 +0100

    read from input and put in output

--- statement_cleaner.py
+++ statement_cleaner.py
@@ -2,9 +2,10 @@ import os
 import pandas as pd
 import openai
 
-# Set up constants (input and output filenames)
-INPUT_CSV = "./extracted_statements/election_emails/extracted_statements.csv"
-OUTPUT_CSV = "input_out.csv"
+# Set up the paths for the inputs and outputs directories
+INPUTS_DIR = "inputs"
+OUTPUTS_DIR = "outputs"
+
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
@@ -12,15 +13,55 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 def process_line(line):
     """Process a line of text using the OpenAI API."""
     print("Processing line:", line)
-    return
+    return "Processed " + line  # Placeholder for the actual API response
 
 
-df = pd.read_csv(INPUT_CSV)
+os.makedirs(OUTPUTS_DIR, exist_ok=True)
 
+input_files = [f for f in os.listdir(INPUTS_DIR) if f.endswith(".csv")]
 
-df["processed"] = df["statements"].apply(process_line)
+for input_file in input_files:
+    input_path = os.path.join(INPUTS_DIR, input_file)
+    output_file = f"{os.path.splitext(input_file)[0]}_out.csv"
+    output_path = os.path.join(OUTPUTS_DIR, output_file)
 
+    # Read the input CSV into a DataFrame
+    input_df = pd.read_csv(input_path)
 
-df.to_csv(OUTPUT_CSV, index=False)
+    if "statements" not in input_df.columns:
+        print(f"'statements' column not found in {input_file}. Skipping this file.")
+        continue
 
-print(f"Processed data saved to {OUTPUT_CSV}")
+    # Initialize the output DataFrame
+    if os.path.exists(output_path):
+        output_df = pd.read_csv(output_path)
+        # Ensure 'statements' column exists in output_df
+        if "statements" not in output_df.columns:
+            print(
+                f"'statements' column not found in {output_file}. Creating a new file."
+            )
+            output_df = pd.DataFrame(columns=["statements", "processed"])
+    else:
+        output_df = pd.DataFrame(columns=["statements", "processed"])
+
+    # Get new statements
+    new_statements_mask = ~input_df["statements"].isin(output_df["statements"])
+    new_statements = input_df.loc[new_statements_mask, "statements"]
+
+    # Process new statements
+    if not new_statements.empty:
+        processed_data = [
+            {"statements": line, "processed": process_line(line)}
+            for line in new_statements
+        ]
+
+        new_processed_df = pd.DataFrame(processed_data)
+
+        output_df = pd.concat([output_df, new_processed_df], ignore_index=True)
+
+        output_df.to_csv(output_path, index=False)
+        print(f"Processed new data appended to {output_path}")
+    else:
+        print(f"No new statements to process in {input_file}. Skipping processing.")
+
+print("Processing completed.")
